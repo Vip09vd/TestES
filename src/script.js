e9fbd28e0b90e509a9113d8bb01b30ebffa3363f
Vadim <vip09vd@gmail.com>
2017-03-03 17:33:40 +0200

    WTF!

--- src/script.js
+++ src/script.js
@@ -1,18 +1,21 @@
-function getData(resp) {
-    obj = resp;
-    obj.forEach(function (item, i, obj) {
-        function createElement() {
-            const tbody = document.getElementById('users-body');
-            const newRow = document.createElement('tr');
-            const newTd = document.createElement('td');
-            document.body.insertBefore(newRow, tbody);
-            document.body.insertBefore(newTd, newRow);
-            newTd.innerText = item[0].name;
-        }
-        createElement();
-    });
-};
+const tbody = document.getElementById('users-body');
+function myCreateFunction() {
+    for (let i = 0; i < 10; i++){
+        var row = tbody.insertRow(0);
+        var cell = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        cell2.innerText = 'address';
+        cell.innerText = name;
+        console.log(name);
+    }
+}
+
+myCreateFunction();
 
 
-
+function getData(arr) {
+    for (let obj of arr){
+        console.log(obj.name);
+    }
+}
 
