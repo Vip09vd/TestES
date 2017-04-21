942ba76da52596e5c645b1813c01c5ba1d96510f
Vadim <vip09vd@gmail.com>
2017-04-21 18:30:40 +0300

    vse po novoi7

--- src/script.js
+++ src/script.js
@@ -49,10 +49,11 @@ const root = document.getElementById('root');
 // }
 
 class App {
-    constructor(component, element){
+    constructor(component, element) {
         this.component = component;
         this.element = element;
     }
+
     render() {
         const newChild = this.component.render();
         this.element.innerHTML = '';
@@ -60,25 +61,72 @@ class App {
     }
 }
 
-class InputWithText {
+class Publisher {
     constructor() {
+        this.subscribers = [];
+    }
+
+    subscribe(callback) {
+        this.subscribers.push(callback);
+    }
+
+    publish(data) {
+        this.subscribers.forEach(function (subscriber) {
+            subscriber(data);
+        })
+    }
+}
+
+class InputWithText extends Publisher {
+    constructor() {
+        super();
         this.div = document.createElement('div');
         this.value = '';
+        this.texts = [];
     }
 
     handleInput = (event) => {
         this.value = event.target.value;
-        publisher.publish();
+        this.publish('text_changed', this.value);
+    };
+
+    handleSave = () => {
+        if (this.value && this.texts.indexOf(this.value) === -1) {
+            this.texts.push(this.value);
+            this.publish('texts_changed', this.texts);
+        }
     };
 
     render() {
-        this.div.innerHTML = '';
-        const text = new Text('input');
-        const input = new Input('text', this.handleInput, this.value);
-        const text2 = new Text(this.value);
-        this.div.appendChild(text.render());
-        this.div.appendChild(input.render());
-        this.div.appendChild(text2.render());
+        if (!this.text) {
+            this.text = new Text('input');
+            this.div.appendChild(this.text.render());
+        }
+
+        if (!this.input) {
+            this.input = new Input('text', this.handleInput, this.value);
+            this.div.appendChild(this.input.render());
+        }
+
+        if (!this.saveBtn) {
+            this.saveBtn = new Button(this.handleSave);
+            this.div.appendChild(this.saveBtn.render());
+        }
+
+        if (!this.text2) {
+            this.text2 = new Text(this.value);
+            this.subscribe('text_changed', this.text2.update.bind(this.text2));
+            this.div.appendChild(this.text2.render());
+        }
+
+        if (!this.texts) {
+            this.texts.forEach(function (text) {
+                let paragraph = document.createElement('p');
+                paragraph.innerText = text;
+                this.div.appendChild(paragraph);
+            });
+        }
+
         return this.div;
     }
 }
@@ -93,11 +141,33 @@ class Text {
     render() {
         return this.div;
     }
+
+    update(newText) {
+        if (this.text !== newText) {
+            this.text = newText;
+            this.div.innerText = this.text;
+        }
+    }
+}
+
+class Button {
+    constructor(action) {
+        this.action = action;
+        this.button = document.createElement('button');
+        this.button.innerText = 'SAVE';
+        this.button.addEventListener('click', action);
+    }
+
+    render() {
+        return this.button;
+    }
+
 }
 
 class Input {
     constructor(type, onInput, value) {
         this.type = type;
+        this.value = value;
         this.onInput = onInput;
         this.input = document.createElement('input');
         this.input.setAttribute('type', this.type);
@@ -108,25 +178,25 @@ class Input {
     render() {
         return this.input;
     }
+
+    // update(newType, newOnInput, newValue) {
+    //     if (this.type !== newType) {
+    //         this.type = newType;
+    //         this.input.setAttribute('type', this.type);
+    //     }
+    //     if (this.value !== newValue) {
+    //         this.value = newType;
+    //         setTimeout(() => {
+    //             this.input.focus();
+    //         }, 0);
+    //     }
+    //     if (this.onInput !== newOnInput) {
+    //         this.onInput = newOnInput;
+    //     }
+    // }
 }
 
-class Publisher {
-    constructor() {
-        this.subscribers = [];
-    }
 
-    subscribe(callback) {
-        this.subscribers.push(callback);
-    }
-
-    publish() {
-        this.subscribers.forEach(function (subscriber) {
-            subscriber();
-        })
-    }
-}
-
-const publisher = new Publisher();
-const app = new App(new InputWithText(), root);
+const inputWithText = new InputWithText();
+const app = new App(inputWithText, root);
 app.render();
-publisher.subscribe(app.render.bind(app));
